62dca40214cc04f13a05abccf71c3325212b7d11
Chris Hunt <chris.hunt@ticketmaster.com>
2016-04-25 15:02:43 -0400

    Formats demos

---
+++ DemoFormats.ps1
@@ -0,0 +1,16 @@
+﻿function global:prompt { Write-host ""; return 'PowerShellHumanizer> ' }
+
+function newline { Start-Sleep -Milliseconds 2000; Write-Host ''}
+cls
+Import-Module PowerShellHumanizer
+Write-Host "`tPowerShell Humanizer Demo : Custom Formats`t" -ForegroundColor White -BackgroundColor DarkBlue
+newline
+<#
+newline
+Write-Host 'PowerShellHumanizer> [TimeSpan]::new(1,0,0)'
+[TimeSpan]::new(1,0,0)
+newline
+Write-Host 'PowerShellHumanizer> ls'
+ls
+Start-Sleep -Milliseconds 900
+newline#>
