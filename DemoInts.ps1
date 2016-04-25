400ec7ee386b55454f3aafd661440f9242f6efaa
Chris Hunt <chris.hunt@ticketmaster.com>
2016-04-25 14:39:22 -0400

    Integer Demos

---
+++ DemoInts.ps1
@@ -0,0 +1,18 @@
+﻿function global:prompt { Write-host ""; return 'PowerShellHumanizer> ' }
+
+function newline { Start-Sleep -Milliseconds 900; Write-Host ''}
+cls
+Import-Module PowerShellHumanizer
+Write-Host "`tPowerShell Humanizer Demo : Integer Extension Methods `t" -ForegroundColor White -BackgroundColor DarkBlue
+newline
+newline
+Write-Host 'PowerShellHumanizer> (3).Ordinalize()'
+(3).Ordinalize()
+newline
+Write-Host 'PowerShellHumanizer> (3).ToWords()'
+(3).ToWords()
+newline
+Write-Host 'PowerShellHumanizer> (Get-Date 2/13/2016) + (3).Weeks'
+(Get-Date 2/13/2016) + (3).Weeks
+newline
+Start-Sleep -Milliseconds 900
