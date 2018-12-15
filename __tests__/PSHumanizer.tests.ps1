4e3663f86f04dff3ff178d59bf42ad7925ba3de7
dfinke <finked@hotmail.com>
2018-12-15 10:54:39 -0500

    make test fail

--- __tests__/PSHumanizer.tests.ps1
+++ __tests__/PSHumanizer.tests.ps1
@@ -1,7 +1,7 @@
 Import-Module $PSScriptRoot\..\PowerShellHumanizer.psd1 -Force
 
 Describe "Test Humanizer" {
-    It "Flight test" {
-        $true | Should Be $true
+    It "Flight test should be true" {
+        $true | Should Be $false
     }
 }
