f972bacb043db53df3de6c08afc04988abc0078d
dfinke <finked@hotmail.com>
2020-04-29 16:41:00 -0400

    Remove param stmt

--- CustomTypes.ps1
+++ CustomTypes.ps1
@@ -1,29 +1,19 @@
 Update-TypeData -Force -TypeName String -MemberType ScriptMethod -MemberName Capitalize -Value {
-    param($propertyName, $lower, $upper)
- 
-    [Humanizer.To]::Transform($this, [Humanizer.To]::SentenceCase   )
+    [Humanizer.To]::Transform($this, [Humanizer.To]::SentenceCase)
 }
 
 Update-TypeData -Force -TypeName String -MemberType ScriptMethod -MemberName Title -Value {
-    param($propertyName, $lower, $upper)
- 
     [Humanizer.To]::Transform($this, [Humanizer.To]::TitleCase)
 }
 
 Update-TypeData -Force -TypeName String -MemberType ScriptMethod -MemberName Strip -Value {
-    param($propertyName, $lower, $upper)
- 
     $this.Trim()
 }
 
 Update-TypeData -Force -TypeName String -MemberType ScriptMethod -MemberName RStrip -Value {
-    param($propertyName, $lower, $upper)
- 
     $this.TrimEnd()
 }
 
 Update-TypeData -Force -TypeName String -MemberType ScriptMethod -MemberName LStrip -Value {
-    param($propertyName, $lower, $upper)
-
     $this.TrimStart()
 }
